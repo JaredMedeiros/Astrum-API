049cdf6b425921f5716b8a5b62f58f1d0414cddd
JaredMedeiros <97901520+JaredMedeiros@users.noreply.github.com>
2022-04-01 20:58:35 -0400

    refactored the signup and login functions, they are now functioning

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use(cors());
 
-app.use('/api/user', require('./routes/userRoutes'))
+app.use('/user', require('./routes/userRoutes'))
 
 app.use(errorHandler)
 
