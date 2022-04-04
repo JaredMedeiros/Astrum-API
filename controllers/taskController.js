bc4e2780bf1c9f1a5b973f0254d714d6e8b7059c
JaredMedeiros <97901520+JaredMedeiros@users.noreply.github.com>
2022-04-04 09:19:52 -0400

    task mvc seems to be working. Tested with postman, but needs to be confirmed with front end test

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -9,23 +9,22 @@ const getTasks = asyncHandler(async (req,res) => {
 
 })
 
-const findById = (req,res) => {
-    User.findOne({_id: req.params.id})
-        .then(user => res.json(user))
-        .catch(err => res.json(err));
-}
+// const getOneTask = (req,res) => {
+//     Project.findOne({taskName req.params.id})
+//         .then(user => res.json(user))
+//         .catch(err => res.json(err));
+// }
 
 const setTask = asyncHandler(async (req, res) => {
-    if(!req.body.tasks.taskName || !req.body.tasks.taskDescription) {
+    if(!req.body.taskName || !req.body.taskDescription) {
         res.status(400).send('Please add valid task')
     }
 
     const task = await Project.findOneAndUpdate(
-        {_id: req.id},
-        {$set:
-        {"tasks.$.taskName": req.body.tasks.taskName,
-        "tasks.$.taskDescription": req.body.tasks.taskDescription
-        }}
+        {user: req.user.id},
+        {tasks: {taskName : req.body.taskName, taskDescription: req.body.taskDescription}
+        },
+        {upsert: true, new: true}
         
     )   
     res.status(200).json(task)
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -4,7 +4,7 @@ const { getTasks, setTask, findById } =require('../controllers/taskController');
 const { protect } = require('../middleware/authMiddleware')
 
 router.get("/", protect, getTasks);
-router.get("/:id", protect, findById)
+// router.get("/:id", protect, findById)
 router.post("/", protect, setTask);
 
 module.exports = router;
